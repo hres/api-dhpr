b14a7149ac9f031898924754f0acf17c5b3164b9
jlin044 <jlin044@uottawa.ca>
2016-11-29 10:36:45 -0500

    Create AerGender.cs

---
+++ dhprWebApi/Models/AerGender.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DhprWebApi.Models
+{
+    public class AerGender
+    {
+        public int id { get; set; }
+		public int code { get; set; }
+		public String gender { get; set; }
+		public String language { get; set; }
+
+    }
+
+
+}
