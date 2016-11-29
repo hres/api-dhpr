2eea6f470b05c50774a884b80fb510a6c080d7e3
jlin044 <jlin044@uottawa.ca>
2016-11-29 10:46:45 -0500

    Update Ci.cs

--- dhprWebApi/Models/Ci.cs
+++ dhprWebApi/Models/Ci.cs
@@ -8,19 +8,19 @@ namespace dhprWebApi.Models
     public class Ci
     {
         public int id { get; set; }
-		public String ci_usedfor { get; set; }
-		public String ci_whatitdoes { get; set; }
-		public String ci_whennotused { get; set; }
-		public String ci_medicinalingredients { get; set; }
-		public String ci_nonmedicinalingredients { get; set; }
+		public String ci_used_for { get; set; }
+		public String ci_what_it_does { get; set; }
+		public String ci_when_not_used { get; set; }
+		public String ci_medicinal_ingredients { get; set; }
+		public String ci_nonmedicinal_ingredients { get; set; }
 		public String ci_dosage { get; set; }
 		public String ci_warnings { get; set; }
 		public String ci_interactions { get; set; }
-		public String ci_properuse { get; set; }
-		public String ci_sideeffects { get; set; }
+		public String ci_proper_use { get; set; }
+		public String ci_side_effects { get; set; }
 		public String ci_storage { get; set; }
-		public String ci_reportingsideeffects { get; set; }
-		public String ci_moreinfo { get; set; }
+		public String ci_reporting_side_effects { get; set; }
+		public String ci_more_info { get; set; }
 		public String ci_language { get; set; }
 		public int ci_id { get; set; }
 		public int product_monograph_id { get; set; }
