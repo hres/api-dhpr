6353f9336e9720ef16174d24ffc0f894104b4885
jlin044 <jlin044@uottawa.ca>
2016-11-29 10:43:15 -0500

    Update AerLink.cs

--- dhprWebApi/Models/AerLink.cs
+++ dhprWebApi/Models/AerLink.cs
@@ -8,7 +8,7 @@ namespace dhprWebApi.Models
     public class AerLink
     {
         public int id { get; set; }
-		public int reportid { get; set; }
+		public int report_id { get; set; }
 		public String type { get; set; }
 		public int linked_aer_number { get; set; }
 		public String language { get; set; }
