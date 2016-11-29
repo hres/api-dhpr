88c894dbbb701d6c013d4cadd9f86f65e6fab0a0
jlin044 <jlin044@uottawa.ca>
2016-11-29 10:42:09 -0500

    Update Aer.cs

--- dhprWebApi/Models/Aer.cs
+++ dhprWebApi/Models/Aer.cs
@@ -8,33 +8,33 @@ namespace dhprWebApi.Models
     public class Aer
     {
         public int id { get; set; }
-		public int reportid { get; set; }
-		public int latestaerversionnumber { get; set; }
-		public string marketauthorizationholderaernumber { get; set; }
-		public DateTime? initialreceiveddate { get; set; }
-		public DateTime? latestreceiveddate { get; set; }
-		public string typeofreport { get; set; }
-		public int seriousreport { get; set; }
-		public string agegroup { get; set; }
+		public int report_id { get; set; }
+		public int latest_aer_version_number { get; set; }
+		public string market_authorization_holder_aer_number { get; set; }
+		public DateTime? initial_received_date { get; set; }
+		public DateTime? latest_received_date { get; set; }
+		public string type_of_report { get; set; }
+		public int serious_report { get; set; }
+		public string age_group { get; set; }
 		public int age { get; set; }
-		public string ageunit { get; set; }
+		public string age_unit { get; set; }
 		public long age_years { get; set; }
 		public int gender { get; set; }
 		public int weight { get; set; }
-		public string weightunit { get; set; }
+		public string weight_unit { get; set; }
 		public int height { get; set; }
-		public string heightunit { get; set; }
-		public string reportoutcome { get; set; }
-		public string reportertype { get; set; }
-		public string sourceofreport { get; set; }
+		public string height_unit { get; set; }
+		public string report_outcome { get; set; }
+		public string reporter_type { get; set; }
+		public string source_of_report { get; set; }
 		public int death { get; set; }
 		public int disability { get; set; }
-		public int congenitalanomaly { get; set; }
-		public int lifethreatening { get; set; }
+		public int congenital_anomaly { get; set; }
+		public int life_threatening { get; set; }
 		public int hospitalization { get; set; }
-		public string othermedicallyimportantconditions { get; set; }
-		public string recordtype { get; set; }
-		public string linkaernumber { get; set; }
+		public string other_medically_important_conditions { get; set; }
+		public string record_type { get; set; }
+		public string link_aer_number { get; set; }
 		public string language { get; set; }
 
     }
